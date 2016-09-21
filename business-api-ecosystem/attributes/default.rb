609ca99759031053c45e2332c436c99f7429b171
Ubuntu <fdelavega@conwet.com>
2016-09-21 13:57:21 +0000

    Update glassfish version

--- business-api-ecosystem/attributes/default.rb
+++ business-api-ecosystem/attributes/default.rb
@@ -44,8 +44,8 @@ default[:java][:jdk_version] = '8'
 default[:java][:oracle][:accept_oracle_download_terms] = true
 
 # Glassfish
-default[:glassfish][:version] = '4.1.1'
-default[:glassfish][:package_url] = 'http://download.java.net/glassfish/4.1.1/release/glassfish-4.1.1.zip'
+default[:glassfish][:version] = '4.1'
+default[:glassfish][:package_url] = 'http://download.java.net/glassfish/4.1/release/glassfish-4.1.zip'
 default[:glassfish][:domains][:domain1][:config][:username] = 'admin'
 default[:glassfish][:domains][:domain1][:config][:password] = 'adminpwd'
 default[:glassfish][:domains][:domain1][:config][:master_password] = 'masterpwd'
